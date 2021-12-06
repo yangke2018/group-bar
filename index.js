d6e742a02368421fb490778d85a5de7d475bfbfc
yangke.2020 <yangke.2020@bytedance.com>
2021-12-06 18:53:58 +0800

    初始化项目

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-// const Demo = require("./lib/index")
-import testDemo from '@group-bar'
-// const testDemo = require("@group-bar")
+import * as d3 from "https://cdn.skypack.dev/d3@7";
+class groupBar {
+    constructor(props) {
+        const {
+            container,
+            gapHeight,
+            uType,
+            width
+        } = props
+        console.log('container', container)
+        this.gapHeight = gapHeight
+        this.uType = uType
+        this.width = width
+        this.container = container
+    }
+    init() {
+        var svg = d3.select(this.container)
+            .append('svg')
+            .style('background', '#eeeeee')
+            .attr('width', this.width + 'px')
+            .attr('height', this.gapHeight * (this.uType + 1) + 'px')
+            .style("border", '1px solid #eee')
+    }
+}
 
-console.log('testDemo',testDemo)
-// module.exports = {Demo}
+export default groupBar
