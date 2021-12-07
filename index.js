1b67052568610a099a1a01485ce936970d5f9cd6
yangke.2020 <yangke.2020@bytedance.com>
2021-12-07 18:34:14 +0800

    引入rollup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7";
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
+import * as d3 from "d3";
+
 class groupBar {
     constructor(props) {
         const {
